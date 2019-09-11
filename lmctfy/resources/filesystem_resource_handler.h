0fb1a7c1d37feb2d11c8fedf2335a87afe03d7ae
max19931 <53226461+max19931@users.noreply.github.com>
2019-09-11 14:59:34 +0200

    Clean up

--- lmctfy/resources/filesystem_resource_handler.h
+++ lmctfy/resources/filesystem_resource_handler.h
@@ -1,17 +1,3 @@
-// Copyright 2014 Google Inc. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 #ifndef SRC_RESOURCES_FILESYSTEM_RESOURCE_HANDLER_H_
 #define SRC_RESOURCES_FILESYSTEM_RESOURCE_HANDLER_H_
 
@@ -38,52 +24,28 @@ typedef ::system_api::KernelAPI KernelApi;
 
 class FilesystemResourceHandlerFactory : public CgroupResourceHandlerFactory {
  public:
-  // Create an instance of this factory. If the resource is not supported on
-  // this machine a NOT_FOUND error is returned. Does not take ownership of
-  // any argument.
-  static ::util::StatusOr<FilesystemResourceHandlerFactory *> New(
-      CgroupFactory *cgroup_factory, const KernelApi *kernel,
-      EventFdNotifications *eventfd_notifications);
-
-  // Takes ownership of rlimit_controller_factory. Does not own
-  // cgroup_factory or kernel.
-  FilesystemResourceHandlerFactory(
-      const RLimitControllerFactory *rlimit_controller_factory,
-      CgroupFactory *cgroup_factory,
-      const KernelApi *kernel);
+  static ::util::StatusOr<FilesystemResourceHandlerFactory *> New(CgroupFactory *cgroup_factory, const KernelApi *kernel,EventFdNotifications *eventfd_notifications);
+  FilesystemResourceHandlerFactory(const RLimitControllerFactory *rlimit_controller_factory,CgroupFactory *cgroup_factory,const KernelApi *kernel);
   virtual ~FilesystemResourceHandlerFactory() {}
 
  protected:
-  virtual ::util::StatusOr<ResourceHandler *> GetResourceHandler(
-      const string &container_name) const;
-  virtual ::util::StatusOr<ResourceHandler *> CreateResourceHandler(
-      const string &container_name, const ContainerSpec &spec) const;
+  virtual ::util::StatusOr<ResourceHandler *> GetResourceHandler(const string &container_name) const;
+  virtual ::util::StatusOr<ResourceHandler *> CreateResourceHandler(const string &container_name, const ContainerSpec &spec) const;
 
  private:
   string GetEffectiveContainerName(const string &container_name) const;
-
-  // Controller factory for rlimit cgroup controllers.
-  const ::std::unique_ptr<const RLimitControllerFactory>
-      rlimit_controller_factory_;
-
+  const ::std::unique_ptr<const RLimitControllerFactory> rlimit_controller_factory_;
   friend class FilesystemResourceHandlerFactoryTest;
   DISALLOW_COPY_AND_ASSIGN(FilesystemResourceHandlerFactory);
 };
 
 class FilesystemResourceHandler : public CgroupResourceHandler {
  public:
-  // Does not own kernel. Takes ownership of rlimit_controller.
-  FilesystemResourceHandler(
-      const string &container_name,
-      const KernelApi *kernel,
-      RLimitController *rlimit_controller);
+  FilesystemResourceHandler(const string &container_name,const KernelApi *kernel,RLimitController *rlimit_controller);
   virtual ~FilesystemResourceHandler() {}
-
-  virtual ::util::Status Stats(Container::StatsType type,
-                               ContainerStats *output) const;
+  virtual ::util::Status Stats(Container::StatsType type,ContainerStats *output) const;
   virtual ::util::Status Spec(ContainerSpec *spec) const;
-  virtual ::util::StatusOr<Container::NotificationId> RegisterNotification(
-      const EventSpec &spec, Callback1< ::util::Status> *callback);
+  virtual ::util::StatusOr<Container::NotificationId> RegisterNotification(const EventSpec &spec, Callback1< ::util::Status> *callback);
 
   virtual ::util::Status DoUpdate(const ContainerSpec &spec);
   virtual void RecursiveFillDefaults(ContainerSpec *spec) const;
